b324794ba6eb355f53b9093d6cdf74d8bcb1d8e6
Paweł Antczak <pawel@antczak.org>
2013-08-27 23:46:48 +0200

    Master/detials animation on one pane mode + flow update.
    Former-commit-id: 7dd741719fe17074dd90bc3eb59bceff7a8b8d4e [formerly 0afecc4ffebde26a56caf2e77563b768128c77ed]
    Former-commit-id: 851907f65d311fef84253318bd65a29bec814589

--- src/org/antczak/whereIsMyPackage/MainFragment.java
+++ src/org/antczak/whereIsMyPackage/MainFragment.java
@@ -1,3 +1,4 @@
+
 package org.antczak.whereIsMyPackage;
 
 import org.antczak.whereIsMyPackage.dummy.DummyContent;
@@ -20,172 +21,169 @@ import com.tjerkw.slideexpandable.library.SlideExpandableListAdapter;
 
 public class MainFragment extends Fragment implements OnActionClickListener, OnItemClickListener {
 
-	private final String TAG = "MainFragment";
+    private final String TAG = "MainFragment";
 
-	public static final String FRAGMENT_TAG = "MainFragment";
-	
-	private ActionSlideExpandableListView mPackagesList;
+    public static final String FRAGMENT_TAG = "MainFragment";
 
-	/**
-	 * The serialization (saved instance state) Bundle key representing the
-	 * activated item position. Only used on tablets.
-	 */
-	private static final String ACTIV_POSITION = "activated_position";
+    private ActionSlideExpandableListView mPackagesList;
 
-	/**
-	 * The fragment's current callback object, which is notified of list item
-	 * clicks.
-	 */
-	private Callbacks mCallbacks = sDummyCallbacks;
+    /**
+     * The serialization (saved instance state) Bundle key representing the
+     * activated item position. Only used on tablets.
+     */
+    private static final String ACTIV_POSITION = "activated_position";
 
-	/**
-	 * The current activated item position. Only used on tablets.
-	 */
-	private int mActivatedPosition = ListView.INVALID_POSITION;
+    /**
+     * The fragment's current callback object, which is notified of list item
+     * clicks.
+     */
+    private Callbacks mCallbacks = sDummyCallbacks;
 
-	private boolean mActivateOnItemClick = false;
+    /**
+     * The current activated item position. Only used on tablets.
+     */
+    private int mActivatedPosition = ListView.INVALID_POSITION;
 
-	/**
-	 * A callback interface that all activities containing this fragment must
-	 * implement. This mechanism allows activities to be notified of item
-	 * selections.
-	 */
-	public interface Callbacks {
-		/**
-		 * Callback for when an item has been selected.
-		 */
-		public void onItemSelected(String id);
-	}
+    private boolean mActivateOnItemClick = false;
 
-	/**
-	 * A dummy implementation of the {@link Callbacks} interface that does
-	 * nothing. Used only when this fragment is not attached to an activity.
-	 */
-	private static Callbacks sDummyCallbacks = new Callbacks() {
-		@Override
-		public void onItemSelected(String id) {
-		}
-	};
+    /**
+     * A callback interface that all activities containing this fragment must
+     * implement. This mechanism allows activities to be notified of item
+     * selections.
+     */
+    public interface Callbacks {
+        /**
+         * Callback for when an item has been selected.
+         */
+        public void onItemSelected(String id);
+    }
 
-	/**
-	 * Mandatory empty constructor for the fragment manager to instantiate the
-	 * fragment (e.g. upon screen orientation changes).
-	 */
-	public MainFragment() {
-		Log.d(TAG, "I'm main fragment.");
-		Log.d(TAG, "mActivateOnItemClick:" + mActivateOnItemClick);
-	}
+    /**
+     * A dummy implementation of the {@link Callbacks} interface that does
+     * nothing. Used only when this fragment is not attached to an activity.
+     */
+    private static Callbacks sDummyCallbacks = new Callbacks() {
+        @Override
+        public void onItemSelected(String id) {
+        }
+    };
 
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
+    /**
+     * Mandatory empty constructor for the fragment manager to instantiate the
+     * fragment (e.g. upon screen orientation changes).
+     */
+    public MainFragment() {
+        Log.d(TAG, "I'm main fragment.");
+        Log.d(TAG, "mActivateOnItemClick:" + mActivateOnItemClick);
+    }
 
-	}
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
 
-	@Override
-	public View onCreateView(LayoutInflater inflater, ViewGroup container,
-			Bundle savedInstanceState) {
-		View rootView = inflater.inflate(R.layout.fragment_main, container,
-				false);
+    }
 
-		mPackagesList = (ActionSlideExpandableListView) rootView.findViewById(R.id.package_list);
-		ArrayAdapter adapter = new ArrayAdapter<DummyContent.DummyItem>(
-				getActivity(), R.layout.list_item_packages,
-				android.R.id.text1, DummyContent.ITEMS);
-		mPackagesList.setAdapter(
-	            new SlideExpandableListAdapter(
-	                adapter,
-	                R.id.expandable_toggle_button,
-	                R.id.expandable
-	            )
-	        );
-		//mPackagesList.setAdapter(adapter);
-		// TODO: replace with a real list adapter.
-		//mPackagesList.setAdapter();
-		mPackagesList
-				.setChoiceMode(isActivateOnItemClick() ? ListView.CHOICE_MODE_SINGLE
-						: ListView.CHOICE_MODE_NONE);
-		mPackagesList.setItemActionListener(this, R.id.buttonA);
-		//mPackagesList.enableExpandOnItemClick();
-		mPackagesList.setOnItemClickListener(this);
-		
-		return rootView;
-	}
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_main, container,
+                false);
 
-	@Override
-	public void onViewCreated(View view, Bundle savedInstanceState) {
-		super.onViewCreated(view, savedInstanceState);
+        mPackagesList = (ActionSlideExpandableListView) rootView.findViewById(R.id.package_list);
+        ArrayAdapter adapter = new ArrayAdapter<DummyContent.DummyItem>(
+                getActivity(), R.layout.list_item_packages,
+                android.R.id.text1, DummyContent.ITEMS);
+        mPackagesList.setAdapter(
+                new SlideExpandableListAdapter(
+                        adapter,
+                        R.id.expandable_toggle_button,
+                        R.id.expandable
+                )
+                );
+        // mPackagesList.setAdapter(adapter);
+        // TODO: replace with a real list adapter.
+        // mPackagesList.setAdapter();
+        mPackagesList
+                .setChoiceMode(isActivateOnItemClick() ? ListView.CHOICE_MODE_SINGLE
+                        : ListView.CHOICE_MODE_NONE);
+        mPackagesList.setItemActionListener(this, R.id.buttonA);
+        mPackagesList.setOnItemClickListener(this);
 
-		// Restore the previously serialized activated item position.
-		if (savedInstanceState != null
-				&& savedInstanceState.containsKey(ACTIV_POSITION)) {
-			setActivatedPosition(savedInstanceState.getInt(ACTIV_POSITION));
-		}
-	}
+        return rootView;
+    }
 
-	@Override
-	public void onAttach(Activity activity) {
-		super.onAttach(activity);
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
 
-		// Activities containing this fragment must implement its callbacks.
-		if (!(activity instanceof Callbacks)) {
-			throw new IllegalStateException(
-					"Activity must implement fragment's callbacks.");
-		}
+        // Restore the previously serialized activated item position.
+        if (savedInstanceState != null
+                && savedInstanceState.containsKey(ACTIV_POSITION)) {
+            setActivatedPosition(savedInstanceState.getInt(ACTIV_POSITION));
+        }
+    }
 
-		mCallbacks = (Callbacks) activity;
-	}
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
 
-	@Override
-	public void onDetach() {
-		super.onDetach();
+        // Activities containing this fragment must implement its callbacks.
+        if (!(activity instanceof Callbacks)) {
+            throw new IllegalStateException(
+                    "Activity must implement fragment's callbacks.");
+        }
 
-		// Reset the active callbacks interface to the dummy implementation.
-		mCallbacks = sDummyCallbacks;
-	}
+        mCallbacks = (Callbacks) activity;
+    }
 
+    @Override
+    public void onDetach() {
+        super.onDetach();
 
+        // Reset the active callbacks interface to the dummy implementation.
+        mCallbacks = sDummyCallbacks;
+    }
 
-	@Override
-	public void onSaveInstanceState(Bundle outState) {
-		super.onSaveInstanceState(outState);
-		if (mActivatedPosition != ListView.INVALID_POSITION) {
-			// Serialize and persist the activated item position.
-			outState.putInt(ACTIV_POSITION, mActivatedPosition);
-		}
-	}
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        if (mActivatedPosition != ListView.INVALID_POSITION) {
+            // Serialize and persist the activated item position.
+            outState.putInt(ACTIV_POSITION, mActivatedPosition);
+        }
+    }
 
-	private void setActivatedPosition(int position) {
-		if (position == ListView.INVALID_POSITION) {
-			mPackagesList.setItemChecked(mActivatedPosition, false);
-		} else {
-			mPackagesList.setItemChecked(position, true);
-		}
+    private void setActivatedPosition(int position) {
+        if (position == ListView.INVALID_POSITION) {
+            mPackagesList.setItemChecked(mActivatedPosition, false);
+        } else {
+            mPackagesList.setItemChecked(position, true);
+        }
 
-		mActivatedPosition = position;
-	}
+        mActivatedPosition = position;
+    }
 
-	public boolean isActivateOnItemClick() {
-		Log.d(TAG, "isActivateOnItemClick:" + mActivateOnItemClick);
-		return mActivateOnItemClick;
-	}
+    public boolean isActivateOnItemClick() {
+        Log.d(TAG, "isActivateOnItemClick:" + mActivateOnItemClick);
+        return mActivateOnItemClick;
+    }
 
-	public void setActivateOnItemClick(boolean activateOnItemClick) {
-		Log.d(TAG, "setActivateOnItemClick:" + activateOnItemClick);
-		this.mActivateOnItemClick = activateOnItemClick;
-	}
+    public void setActivateOnItemClick(boolean activateOnItemClick) {
+        Log.d(TAG, "setActivateOnItemClick:" + activateOnItemClick);
+        this.mActivateOnItemClick = activateOnItemClick;
+    }
 
-	@Override
-	public void onClick(View itemView, View clickedView, int position) {
-		Log.d(TAG, "onClick: " + position);
-		
-	}
+    @Override
+    public void onClick(View itemView, View clickedView, int position) {
+        Log.d(TAG, "onClick: " + position);
 
-	@Override
-	public void onItemClick(AdapterView<?> arg0, View arg1, int arg2, long arg3) {
-		Log.d(TAG, "onItemClick: " + arg2);
-		mCallbacks.onItemSelected(DummyContent.ITEMS.get(arg2).id);
-		
-	}
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> arg0, View arg1, int arg2, long arg3) {
+        Log.d(TAG, "onItemClick: " + arg2);
+        mCallbacks.onItemSelected(DummyContent.ITEMS.get(arg2).id);
+
+    }
 
 }
