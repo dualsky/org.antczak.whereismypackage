149edb8c888afe8a025a2d0e6bcc02aee7a35a0a
Paweł Antczak <pawel@antczak.org>
2013-08-29 23:44:55 +0200

    Dragable, sortable tags list
    Former-commit-id: d0f623e1637acfcd7931ebaf65ca2be1da90d3ef [formerly 115118b692abd3e7ce776266f63dff933dd91f00]
    Former-commit-id: 636b119c75318ade7c8392d7531abf3ef03ebedd

--- src/org/antczak/whereIsMyPackage/SlidingMenuFragment.java
+++ src/org/antczak/whereIsMyPackage/SlidingMenuFragment.java
@@ -1,3 +1,4 @@
+
 package org.antczak.whereIsMyPackage;
 
 import android.os.Bundle;
@@ -5,30 +6,40 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.TextView;
+
+import com.mobeta.android.dslv.DragSortListView;
 
 import org.antczak.whereIsMyPackage.dummy.DummyContent;
 
 public class SlidingMenuFragment extends Fragment {
 
-	public SlidingMenuFragment() {
-	}
+    private DragSortListView mTagsList;
 
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
+    public SlidingMenuFragment() {
+    }
 
-	}
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
 
-	@Override
-	public View onCreateView(LayoutInflater inflater, ViewGroup container,
-			Bundle savedInstanceState) {
-		View rootView = inflater.inflate(R.layout.fragment_slidingmenu, container,
-				false);
+    }
 
-		((TextView) rootView.findViewById(R.id.package_detail))
-				.setText("I'm sliding menu");
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_slidingmenu, container,
+                false);
 
-		return rootView;
-	}
+        ArrayAdapter adapter = new ArrayAdapter<DummyContent.DummyItem>(
+                getActivity(), R.layout.list_item_tags,
+                android.R.id.text1, DummyContent.ITEMS);
+
+        mTagsList = (DragSortListView) rootView.findViewById(R.id.folders_list);
+        mTagsList.setAdapter(adapter);
+        mTagsList.setDragEnabled(true);
+
+        return rootView;
+    }
 }
